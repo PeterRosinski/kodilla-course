ceee3478c16f06fe7f89a84a0b447f2da1efabb5
PeterRosinski <rosinski.piotr@gmail.com>
2018-02-11 14:03:39 +0100

    Modul 6.2.01

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -18,13 +18,13 @@ public class TestingMain {
         int a=756;
         int b=457;
         int resultAdd = calculator.add(a,b);
-        if (resultAdd==a+b) {
+        if (resultAdd==1213) {
             System.out.println("Test calculator.add OK");
         } else {
             System.out.println("Test calculator.add Error!");
         }
         int resultSubtract = calculator.subtract(a,b);
-        if (resultSubtract==a-b) {
+        if (resultSubtract==299) {
             System.out.println("Test calculator.subtract OK");
         } else {
             System.out.println("Test calculator.subtract Error!");
