1d6f6329ccb5bc9388d41d78820a6417463996a3
PeterRosinski <rosinski.piotr@gmail.com>
2018-02-15 22:45:49 +0100

    Modul 6.6.tdd

--- kodilla-testing/src/main/java/com/kodilla/testing/library/BookLibrary.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/library/BookLibrary.java
@@ -1,4 +1,37 @@
 package com.kodilla.testing.library;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookLibrary {
+
+    LibraryDatabase libraryDatabase;
+
+    public BookLibrary(LibraryDatabase libraryDatabase) {
+        this.libraryDatabase = libraryDatabase;
+    }
+
+    public List<Book> listBooksWithCondition(String titleFragment) {
+        List<Book> bookList = new ArrayList<Book>();
+        if (titleFragment.length() < 3) return bookList;
+        List<Book> resultList = libraryDatabase
+                .listBooksWithCondition(titleFragment);
+        if (resultList.size() > 20) return bookList;
+        bookList = resultList;
+        return bookList;
+    }
+
+    public List<Book> listBooksInHandsOf(LibraryUser libraryUser) {
+        List<Book> bookList = new ArrayList<Book>();
+        Book book1 = new Book("The book title", "The book author", 2000);
+        Book book2 = new Book("The book title", "The book author", 2001);
+        Book book3 = new Book("The book title", "The book author", 2002);
+        bookList.add(book1);
+        bookList.add(book2);
+        bookList.add(book3);
+
+        //temporary returning list of one book
+        return bookList;
+    }
+
 }
