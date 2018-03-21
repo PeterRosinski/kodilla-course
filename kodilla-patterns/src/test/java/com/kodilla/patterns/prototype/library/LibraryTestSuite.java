e9b5045a90dc4401d352376d4c1f9c65dfeb0b53
PeterRosinski <rosinski.piotr@gmail.com>
2018-03-21 10:27:52 +0100

    Moduł 11.2.01

--- kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -1,6 +1,7 @@
 package com.kodilla.patterns.prototype.library;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.time.LocalDate;
@@ -8,18 +9,22 @@ import java.util.stream.IntStream;
 
 public class LibraryTestSuite {
 
-    @Test
-    public void testGetBooks() {
+    private static Library library;
+    private static Book book;
+    private static Library clonedLibrary;
+    private static Library deepClonedLibrary;
 
-        Library library = new Library("Library number 1");
+    @BeforeClass
+    public static void declareLibrary() {
+        library = new Library("Library number 1");
         IntStream.iterate(1, n -> n + 1)
                 .limit(9)
                 .forEach(n -> library.getBooks().add(new Book("Book number " + n, "Author number " + n, LocalDate.of(2000,1,1))));
 
-        Book book = new Book("Book number 10", "Author number 10", LocalDate.of(2000,1,1));
+        book = new Book("Book number 10", "Author number 10", LocalDate.of(2000,1,1));
         library.getBooks().add(book);
 
-        Library clonedLibrary = null;
+        clonedLibrary = null;
         try {
             clonedLibrary =  library.shallowCopy();
             clonedLibrary.setName("Library number 2");
@@ -27,7 +32,7 @@ public class LibraryTestSuite {
             System.out.println(e);
         }
 
-        Library deepClonedLibrary = null;
+        deepClonedLibrary = null;
         try {
             deepClonedLibrary = library.deepCopy();
             deepClonedLibrary.setName("Library number 3");
@@ -37,14 +42,33 @@ public class LibraryTestSuite {
 
         library.getBooks().remove(book);
 
+    }
+
+    @Test
+    public void testGetBooksWithLibrary() {
+
         System.out.println(library);
-        System.out.println(clonedLibrary);
-        System.out.println(deepClonedLibrary);
 
         Assert.assertEquals("Library number 1",library.getName());
         Assert.assertEquals(9,library.getBooks().size());
+
+    }
+
+    @Test
+    public void testGetBooksWithClonedLibrary() {
+
+        System.out.println(clonedLibrary);
+
         Assert.assertEquals("Library number 2",clonedLibrary.getName());
         Assert.assertEquals(9,clonedLibrary.getBooks().size());
+
+    }
+
+    @Test
+    public void testGetBooksWithDeepClonedLibrary() {
+
+        System.out.println(deepClonedLibrary);
+
         Assert.assertEquals("Library number 3",deepClonedLibrary.getName());
         Assert.assertEquals(10,deepClonedLibrary.getBooks().size());
 
