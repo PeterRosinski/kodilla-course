b41e7f8ae5240c99ada2bd6edba20d1012c5e59d
PeterRosinski <rosinski.piotr@gmail.com>
2018-06-06 17:01:27 +0200

    Moduł 20.5.01

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/observer/homework/Observable.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/observer/homework/Observable.java
@@ -1,7 +1,5 @@
 package com.kodilla.patterns2.observer.homework;
 
-import com.kodilla.patterns2.observer.homework.Observer;
-
 public interface Observable {
     void registerObserver(Observer observer);
     void notifyObservers(Task task);
--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/observer/homework/Observer.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/observer/homework/Observer.java
@@ -1,7 +1,5 @@
 package com.kodilla.patterns2.observer.homework;
 
-import com.kodilla.patterns2.observer.forum.ForumTopic;
-
 public interface Observer {
     void update(TaskQueue taskQueue, Task task);
 }
