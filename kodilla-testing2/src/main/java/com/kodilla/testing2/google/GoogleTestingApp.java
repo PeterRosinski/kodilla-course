dfba796d863d3de7b1b4f99c7b23bcb3e9b11d48
PeterRosinski <rosinski.piotr@gmail.com>
2018-06-16 16:09:29 +0200

    Moduł 23.3

--- kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
@@ -1,8 +1,7 @@
 package com.kodilla.testing2.google;
 
-import com.kodilla.testint2.config.WebDriverConfig;
+import com.kodilla.testing2.config.WebDriverConfig;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
