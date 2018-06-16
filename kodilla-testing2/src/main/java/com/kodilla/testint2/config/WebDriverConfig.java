390390a10d3cd991303a2214033f97e96aee19d4
PeterRosinski <rosinski.piotr@gmail.com>
2018-06-16 15:30:19 +0200

    Moduł 23.2

---
+++ kodilla-testing2/src/main/java/com/kodilla/testint2/config/WebDriverConfig.java
@@ -0,0 +1,16 @@
+package com.kodilla.testint2.config;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverConfig {
+    public final static String CHROME = "CHROME_DRIVER";
+    public static WebDriver getDriver(final String driver) {
+        System.setProperty("webdriver.chrome.driver","c:\\Selenium-drivers\\Chrome2\\chromedriver.exe");
+        if (driver.equals(CHROME)) {
+            return new ChromeDriver();
+        } else {
+            return null;
+        }
+    }
+}
